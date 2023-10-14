c2ad864e2a8e24894e8890408d76a817d25a78ae
Crowdbotics <team@crowdbotics.com>
2023-10-14 13:13:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-violet-43941",
+  url: "https://rough-violet-43941.botics.co",
+  api: "https://rough-violet-43941.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
